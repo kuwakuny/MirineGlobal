8be2679dc6d76b47b3b255777134e97632a6af20
kuwakuny <jinkyu6869@gmail.com>
2022-06-27 15:10:52 +0900

    share

--- js/share.js
+++ js/share.js
@@ -1,14 +1,22 @@
-function kakaoShare() {
+const url = 'https://jk12lovetypes.netlify.app';
+
+function setShare() {
+    var resultImg = document.querySelector('#resultImg');
+    var resultAlt = resultImg.firstElementChild.alt;
+    const shareTitle = '십이간지 연애유형 결과'
+    const shareDes = infoList[resultAlt].name;
+    const shareImage = url + 'img/image-' + resultAlt + '.png';
+    const shareURL = url + 'page.result-' + resultAlt + '.html';
+
     Kakao.Link.sendDefault({
         objectType: 'feed',
         content: {
-            title: '오늘의 디저트',
-            description: '아메리카노, 빵, 케익',
-            imageUrl:
-                'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+            title: shareTitle,
+            description: shareDes,
+            imageUrl: shareImage,
             link: {
-                mobileWebUrl: 'https://developers.kakao.com',
-                androidExecutionParams: 'test',
+                mobileWebUrl: shareURL,
+                WebUrl: shareURL,
             },
         },
         itemContent: {
@@ -49,15 +57,10 @@ function kakaoShare() {
         },
         buttons: [
             {
-                title: '웹으로 이동',
+                title: '결과 확인하기',
                 link: {
-                    mobileWebUrl: 'https://developers.kakao.com',
-                },
-            },
-            {
-                title: '앱으로 이동',
-                link: {
-                    mobileWebUrl: 'https://developers.kakao.com',
+                    mobileWebUrl: shareURL,
+                    WebUrl: shareURL
                 },
             },
         ]
